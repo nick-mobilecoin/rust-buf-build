9c85d1f953e2e17dc966639a99a734dde8d08c78
Nick Santana <nick@mobilecoin.com>
2023-02-07 15:34:13 -0800

    This is why git rebase is a horrible horrible thing.
    have a tonic proto building with buf, but not compiling final app

--- build.rs
+++ build.rs
@@ -1,4 +1,29 @@
+use std::process::Command;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
+
+
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    // This could also be read in and output as json
+    let config_file = manifest_dir.join("buf.gen.yaml");
+
+    let mut file = File::create("env_vars.txt").unwrap();
+
+    for (env, value) in std::env::vars() {
+        let line = format!("{env}: {value}\n");
+        file.write(line.as_ref()).unwrap();
+    }
+
+    let mut cmd = Command::new("buf");
+    cmd.arg("generate").arg("buf.build/nick-mc/enclaveapis").arg("--template").arg(config_file);
+    // buf treats output as relative to the output directory
+    cmd.current_dir(env::var("OUT_DIR").unwrap());
+    // tonic_build::compile_protos("proto/helloworld.proto")?;
+    let _ = cmd.output().unwrap();
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,8 @@ use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
 
 pub mod hello_world {
-    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+
+    include!(concat!(env!("OUT_DIR"), concat!("/gen/enclave.v1.rs")));
 }
 
 #[derive(Debug, Default)]
