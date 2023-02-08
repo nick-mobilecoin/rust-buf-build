da2de731f24736c7de9e3b2d7840eb0936a6d24d
Nick Santana <nick@mobilecoin.com>
2023-02-08 08:36:27 -0800

    Landed.
    add note about asocial api in protoc_grpcio

--- build.rs
+++ build.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto_root = "proto";
     println!("cargo:rerun-if-changed={}", proto_root);
+    // Note: That compile_grpc_protos() expects the directory to exist so one
+    // must create subdirectories as needed.
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     protoc_grpcio::compile_grpc_protos(
         &["helloworld.proto"],
