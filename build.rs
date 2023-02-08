0220dcc191c770d3332af06985d21f16bd2fa729
Nick Santana <nick@mobilecoin.com>
2023-02-08 09:47:29 -0800

    I don't know what these changes are supposed to accomplish but somebody told me to make them.
    Working on getting buf.build working with grpcio

--- build.rs
+++ build.rs
@@ -1,23 +1,41 @@
-use std::{env, fs};
-use std::path::PathBuf;
+
+use std::process::Command;
+use std::env;
+use std::env::current_dir;
+use std::fs::File;
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_root = "proto";
-    println!("cargo:rerun-if-changed={}", proto_root);
-    // Note: That compile_grpc_protos() expects the directory to exist so one
-    // must create subdirectories as needed.
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap()).join("gen");
-    fs::create_dir_all(&out_dir).unwrap();
+    dump_env_vars();
 
-    // Creating a wrapping mod due to some issues including the files directly,
-    // see https://github.com/google/flatbuffers/issues/6261
-    fs::write(out_dir.join("mod.rs"), "pub mod helloworld;\npub mod helloworld_grpc;").unwrap();
+    // we could dump this yaml file as json and pass the json string with the
+    // `--template` arg, see <https://docs.buf.build/generate/usage#multiple-templates>
+    // we could also build up the json here and not use a buf.gen.yaml,
+    // this would allow leveraging `OUT_DIR` in the json
+    let cwd = current_dir().unwrap();
+    let config_file = cwd.join("buf.gen.yaml");
 
-    protoc_grpcio::compile_grpc_protos(
-        &["helloworld.proto"],
-        &[proto_root],
-        &out_dir,
-        None
-    ).expect("Failed to compile gRPC definitions!");
+    let mut cmd = Command::new("buf");
+    cmd.arg("generate").arg("buf.build/nick-mc/enclaveapis").arg("--template").arg(&config_file);
+
+    // buf treats output in the `buf.gen.yaml` as relative to the cwd
+    cmd.current_dir(env::var("OUT_DIR").unwrap());
+    let _ = cmd.output().unwrap();
+
+    let mut cmd = Command::new("buf");
+    cmd.arg("generate").arg("buf.build/nick-mc/attestapis").arg("--template").arg(&config_file);
+
+    // buf treats output in the `buf.gen.yaml` as relative to the cwd
+    cmd.current_dir(env::var("OUT_DIR").unwrap());
+    let _ = cmd.output().unwrap();
     Ok(())
+}
+
+/// dirty hacks to see env vars
+fn dump_env_vars() {
+    let mut file = File::create("env_vars.txt").unwrap();
+    for (env, value) in env::vars() {
+        let line = format!("{env}: {value}\n");
+        file.write(line.as_ref()).unwrap();
+    }
 }
